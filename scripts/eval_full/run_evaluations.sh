e1079450fc523b01a7134c47ca60c6644f7c96e9
Vladyslav Usenko <usenko.vladyslav@gmail.com>
2019-04-15 20:57:26 +0000

    Update run_evaluations.sh

--- scripts/eval_full/run_evaluations.sh
+++ scripts/eval_full/run_evaluations.sh
@@ -14,11 +14,11 @@ mkdir $folder_name
 
 
 for d in ${DATASETS[$CI_NODE_INDEX-1]}; do
-   basalt_vio --dataset-path  $DATASET_PATH/$d --cam-calib /usr/etc/basalt/euroc_ds_calib.json \
+   basalt_vio --dataset-path  $DATASET_PATH/$d --cam-calib /usr/etc/basalt/euroc_eucm_calib.json \
         --dataset-type euroc --show-gui 0 --config-path /usr/etc/basalt/euroc_config.json \
         --result-path $folder_name/vio_$d --marg-data eval_tmp_marg_data
 
-    basalt_mapper --show-gui 0 --cam-calib /usr/etc/basalt/euroc_ds_calib.json --marg-data eval_tmp_marg_data \
+    basalt_mapper --show-gui 0 --cam-calib /usr/etc/basalt/euroc_eucm_calib.json --marg-data eval_tmp_marg_data \
         --vocabulary /usr/etc/basalt/orbvoc.dbow3 --result-path $folder_name/mapper_$d
     rm -rf eval_tmp_marg_data
 done
