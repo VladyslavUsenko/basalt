2926020df7feae6423656ffb5c065772a64dc48c
Vladyslav Usenko <vlad.usenko@tum.de>
2020-03-06 12:51:28 +0100

    added tumvi eval

---
+++ scripts/eval_full/gen_results_tumvi.py
@@ -0,0 +1,53 @@
+#!/usr/bin/env python
+
+import os
+import sys
+import json
+
+
+datasets = ['Seq.', 'dataset-corridor1_512_16', 'dataset-magistrale1_512_16', 'dataset-room1_512_16', 'dataset-slides1_512_16']
+
+# Other results.
+
+
+vio = {
+'ate' : ['VIO RMS ATE [m]'],
+'time' : ['VIO Time [s]'],
+'num_frames' : ['VIO Num. Frames']
+}
+
+out_dir = sys.argv[1]
+
+def load_data(x, prefix, key):
+    fname = out_dir + '/' + prefix + '_' + key
+    if os.path.isfile(fname): 
+        with open(fname, 'r') as f:
+            j = json.load(f)
+            res = round(j['rms_ate'], 3)
+            x['ate'].append(float(res))
+            x['time'].append(round(j['exec_time_ns']*1e-9, 3))
+            x['num_frames'].append(j['num_frames'])
+    else:
+        x['ate'].append(float('Inf'))
+        x['time'].append(float('Inf'))
+        x['num_frames'].append(float('Inf'))
+
+
+for key in datasets[1:]:
+    load_data(vio, 'vio', key)
+
+
+row_format ="{:>17}" + "{:>13}" * (len(datasets)-1)
+
+datasets_short = [x[:5] for x in datasets]
+
+print '\nVisual-Inertial Odometry'
+print row_format.format(*datasets_short)
+
+print row_format.format(*vio['ate'])
+#print row_format.format(*vio['time'])
+print row_format.format(*vio['num_frames'])
+
+
+
+
---
+++ scripts/eval_full/run_evaluations_tumvi.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+
+set -e
+set -x
+
+DATASET_PATH=/data/tumvi/512_16/
+
+DATASETS=(
+dataset-corridor1_512_16
+dataset-magistrale1_512_16
+dataset-room1_512_16
+dataset-slides1_512_16
+)
+
+
+folder_name=eval_results_tumvi
+mkdir $folder_name
+
+
+
+for d in ${DATASETS[$CI_NODE_INDEX-1]}; do
+   ../../build/basalt_vio --dataset-path  $DATASET_PATH/$d --cam-calib /usr/etc/basalt/tumvi_512_eucm_calib.json \
+        --dataset-type euroc --show-gui 1 --config-path /usr/etc/basalt/tumvi_512_config.json \
+        --result-path $folder_name/vio_$d --save-trajectory tum
+
+   mv trajectory.txt $folder_name/${d}_basalt_poses.txt
+
+done
+
+#./gen_results_tumvi.py $folder_name > euroc_tumvi.txt
