98d2e831052c1dbcbbf014a95643d1d716a3805f
Vladyslav Usenko <usenko.vladyslav@gmail.com>
2020-04-28 11:56:46 +0200

    updated docker files

--- scripts/clang-format-all.sh
+++ scripts/clang-format-all.sh
@@ -9,7 +9,7 @@ SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 
 FOLDER="${1:-$SCRIPT_DIR/../include $SCRIPT_DIR/../src $SCRIPT_DIR/../test/src}"
 
-CLANG_FORMAT_COMMANDS="clang-format-9 clang-format-8 clang-format"
+CLANG_FORMAT_COMMANDS="clang-format-10 clang-format-9 clang-format-8 clang-format"
 
 # find the first available command:
 for CMD in $CLANG_FORMAT_COMMANDS; do
