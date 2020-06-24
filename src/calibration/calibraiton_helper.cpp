447c1e01d27ca99abc3cece7212578bfa87cee29
Nikolaus Demmel <nikolaus@nikolaus-demmel.de>
2020-06-24 12:18:38 +0200

    silence warnings in 3rd party libs (pangolin, opengv, ros)

--- src/calibration/calibraiton_helper.cpp
+++ src/calibration/calibraiton_helper.cpp
@@ -46,8 +46,12 @@ OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 #include <opengv/relative_pose/methods.hpp>
 
 #include <opengv/sac/Ransac.hpp>
+
+#pragma GCC diagnostic push
+#pragma GCC diagnostic ignored "-Wunused-parameter"
 #include <opengv/sac_problems/absolute_pose/AbsolutePoseSacProblem.hpp>
 #include <opengv/sac_problems/relative_pose/CentralRelativePoseSacProblem.hpp>
+#pragma GCC diagnostic pop
 
 #include <opencv2/calib3d/calib3d.hpp>
 
--- src/utils/keypoints.cpp
+++ src/utils/keypoints.cpp
@@ -43,7 +43,11 @@ OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 #include <opengv/relative_pose/CentralRelativeAdapter.hpp>
 #include <opengv/relative_pose/methods.hpp>
 #include <opengv/sac/Ransac.hpp>
+
+#pragma GCC diagnostic push
+#pragma GCC diagnostic ignored "-Wunused-parameter"
 #include <opengv/sac_problems/relative_pose/CentralRelativePoseSacProblem.hpp>
+#pragma GCC diagnostic pop
 
 namespace basalt {
 
